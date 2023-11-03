767295d25b2c8343195eb8927539bdfeaca4d77d
y-sierkov <apple@MacBook-Pro-apple.local>
2023-11-03 14:11:33 +0200

    Implemented 2 test scenarios

---
+++ src/test/java/org/example/ListofIPhones.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ListofIPhones {
+    public WebDriver driver;
+
+    public ListofIPhones(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy (xpath = "//span[contains(text(),'APPLE iPhone 15 Plus 256GB Green')]")
+    private WebElement iPhone15Plus256GBGreen;
+
+    public WebElement getiPhone15Plus256GBGreen() {
+        return iPhone15Plus256GBGreen;
+    }
+}
